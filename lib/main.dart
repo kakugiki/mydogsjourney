af58407cf185c8806eb55f30248068f0798e3944
Weihui Guo <weihui.guo@outlook.com>
2020-12-25 14:08:21 -0500

    rename main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,150 @@
+import 'dart:collection';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:date_format/date_format.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // debugShowCheckedModeBanner: false,
-      title: 'Flutter Startup',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+    return new MaterialApp(
+      title: 'Generated App',
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  MyHomePage({Key key}) : super(key: key);
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String textToShow = "I like flutter";
+  String category;
+  Queue<String> journey = new Queue<String>();
 
-  void _incrementCounter() {
+  void addTimeStamp(category) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-      if (_counter.isOdd) {
-        textToShow = "Flutter is cool!";
-      }
-      else {
-        textToShow = "I like flutter!";
-      }
+      if (journey.length > 16) journey.removeLast();
+      journey.addFirst(
+          formatDate(DateTime.now(), [mm, '/', dd, ' ', hh, ':', nn, ':', ss]) +
+              ' ( ' +
+              category +
+              ')');
     });
+  }
+
+  void deleteRecord() {
+    setState(() {
+      if (journey.length > 0)
+        journey.removeFirst();
+    });
+  }
+
+  void showAlertDialog() {
+    // set up the buttons
+    Widget cancelButton = FlatButton(
+      child: Text("Cancel"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: Text("Continue"),
+      onPressed: () {
+        deleteRecord();
+        Navigator.of(context).pop();
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Heads-up"),
+      content: Text(
+          "Continue to delete last entry?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('My Dog\'s Journey'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have clicked the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(textToShow)
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            for (var item in journey)
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(item,
+                      style: TextStyle(fontSize: 24, color: Colors.indigo)))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () async => addTimeStamp('Food'),
+            child: Icon(
+              FontAwesomeIcons.bone,
+              color: Colors.orangeAccent,
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: () async => addTimeStamp('Water'),
+            child: Icon(Icons.local_drink_outlined),
+          ),
+          FloatingActionButton(
+            onPressed: () async => addTimeStamp('Pee'),
+            child: Icon(
+              FontAwesomeIcons.tint,
+              color: Colors.yellow,
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: () async => addTimeStamp('Poop'),
+            child: Icon(
+              FontAwesomeIcons.mapMarkerAlt,
+              color: Colors.brown,
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: showAlertDialog,
+            child: Icon(
+              FontAwesomeIcons.timesCircle,
+              color: Colors.red,
+            ),
+            // child: Icon(Icons.clear, color: Colors.red,),
+          )
+        ],
+      ),
     );
   }
 }
